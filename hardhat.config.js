ff478fb6f7ee8d637827499ba5fed25e7b5ca999
Mikhail Melnik <by.zumzoom@gmail.com>
2022-03-16 20:30:37 +0400

    bump deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,16 @@ const networks = require('./hardhat.networks');
 
 module.exports = {
     etherscan: {
-        apiKey: process.env.MAINNET_ETHERSCAN_KEY,
+        apiKey: {
+            mainnet: process.env.MAINNET_ETHERSCAN_KEY,
+            bsc: process.env.BSC_ETHERSCAN_KEY,
+            optimisticEthereum: process.env.OPTIMISTIC_ETHERSCAN_KEY,
+            polygon: process.env.MATIC_ETHERSCAN_KEY,
+            arbitrumOne: process.env.ARBITRUM_ETHERSCAN_KEY,
+            xdai: process.env.XDAI_ETHERSCAN_KEY,
+            avalanche: process.env.AVAX_ETHERSCAN_KEY,
+            kovan: process.env.KOVAN_ETHERSCAN_KEY,
+        },
     },
     solidity: {
         settings: {
