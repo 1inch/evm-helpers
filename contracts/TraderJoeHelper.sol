8438ca569d40e097219aad83e477ec94acead853
Mikhail Melnik <by.zumzoom@gmail.com>
2022-11-24 12:23:22 +0400

    update traderjoe helper

--- contracts/TraderJoeHelper.sol
+++ contracts/TraderJoeHelper.sol
@@ -11,24 +11,32 @@ contract TraderJoeHelper {
         uint256 reserveY;
     }
 
-    function getBins(IJoePair pair, uint24 startBin, uint24 endBin, bool swapY)
+    function getBins(IJoePair pair, uint24 offset, uint24 size)
         external
         view
         returns (BinData[] memory data, uint24 i)
     {
         uint256 counter = 0;
-        uint256 length = endBin - startBin;
-        data = new BinData[](length);
+        data = new BinData[](size);
+        uint24 lastBin = pair.findFirstNonEmptyBinId(type(uint24).max, true);
         for (
-            i = pair.findFirstNonEmptyBinId(startBin, swapY);
-            i < endBin;
-            i = pair.findFirstNonEmptyBinId(i, swapY)
+            i = offset;
+            i < lastBin && counter < size;
+            i = pair.findFirstNonEmptyBinId(i, false)
         ) {
+            (uint256 x, uint256 y) = pair.getBin(i);
+            if (x > 0 || y > 0) {
+                (data[counter].reserveX, data[counter].reserveY) = (x, y);
+                data[counter].id = i;
+                unchecked{ ++counter; }
+            }
+        }
+        if (i == lastBin && counter < size) {
             (data[counter].reserveX, data[counter].reserveY) = pair.getBin(i);
             data[counter].id = i;
             unchecked{ ++counter; }
+            i = 0;
         }
-
         // cut array size down
         assembly {  // solhint-disable-line no-inline-assembly
             mstore(data, counter)
