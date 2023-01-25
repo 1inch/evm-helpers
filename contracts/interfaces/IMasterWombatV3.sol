2e0ffd536ec81640dcad4763833aa0e206935e36
Mikhail Melnik <by.zumzoom@gmail.com>
2023-01-25 08:35:00 +0400

    optimize a bit

---
+++ contracts/interfaces/IMasterWombatV3.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+interface IAsset {
+    function pool() external view returns(address);
+    function underlyingToken() external view returns(address);
+}
+
+interface IPool {
+    function getTokens() external view returns (address[] memory);
+}
+
+interface IMasterWombatV3 {
+    function poolInfoV3(uint256 i) external view
+    returns (
+        address lpToken,
+        address rewarder,
+        uint40 periodFinish,
+        uint128 sumOfFactors,
+        uint128 rewardRate,
+        uint104 accWomPerShare,
+        uint104 accWomPerFactorShare,
+        uint40 lastRewardTimestamp
+    );
+
+    function poolLength() external view returns (uint256);
+}
