279183be8e9e80146315b82feb55e7f14c8f1669
Ilya Petrusenko <ilyapt4@gmail.com>
2023-06-05 14:43:11 +0200

    use gas() in static calls

--- contracts/CurveLlammaHelper.sol
+++ contracts/CurveLlammaHelper.sol
@@ -13,46 +13,46 @@ contract CurveLlammaHelper {
             mstore(res, 0x20)
 
             mstore(in, shl(224, 0x8f8654c5)) // active_band()
-            let success := staticcall(50000, pool, in, 0x04, add(res, 0x40), 0x20)
+            let success := staticcall(gas(), pool, in, 0x04, add(res, 0x40), 0x20)
             if eq(success, 0) {
                 revert(in, 0x04)
             }
 
             mstore(in, shl(224, 0x2eb858e7)) // p_oracle_up(int256)
             mstore(add(in, 4), mload(add(res, 0x40)))
-            success := staticcall(50000, pool, in, 0x24, add(res, 0x60), 0x20)
+            success := staticcall(gas(), pool, in, 0x24, add(res, 0x60), 0x20)
             if eq(success, 0) {
                 revert(in, 0x24)
             }
 
             mstore(in, shl(224, 0xca72a821)) // min_band()
-            success := staticcall(50000, pool, in, 0x04, add(res, 0x80), 0x20)
+            success := staticcall(gas(), pool, in, 0x04, add(res, 0x80), 0x20)
             if eq(success, 0) {
                 revert(in, 0x04)
             }
             min := mload(add(res, 0x80))
 
             mstore(in, shl(224, 0xaaa615fc)) // max_band()
-            success := staticcall(50000, pool, in, 0x04, add(res, 0xA0), 0x20)
+            success := staticcall(gas(), pool, in, 0x04, add(res, 0xA0), 0x20)
             if eq(success, 0) {
                 revert(in, 0x04)
             }
             max := mload(add(res, 0xA0))
 
             mstore(in, shl(224, 0x86fc88d3)) // price_oracle()
-            success := staticcall(500000, pool, in, 0x04, add(res, 0xC0), 0x20)
+            success := staticcall(gas(), pool, in, 0x04, add(res, 0xC0), 0x20)
             if eq(success, 0) {
                 revert(in, 0x04)
             }
 
             mstore(in, shl(224, 0x77c34594)) // dynamic_fee()
-            success := staticcall(500000, pool, in, 0x04, add(res, 0xE0), 0x20)
+            success := staticcall(gas(), pool, in, 0x04, add(res, 0xE0), 0x20)
             if eq(success, 0) {
                 revert(in, 0x04)
             }
 
             mstore(in, shl(224, 0xfee3f7f9)) // admin_fee()
-            success := staticcall(50000, pool, in, 0x04, add(res, 0x100), 0x20)
+            success := staticcall(gas(), pool, in, 0x04, add(res, 0x100), 0x20)
             if eq(success, 0) {
                 revert(in, 0x04)
             }
@@ -63,7 +63,7 @@ contract CurveLlammaHelper {
 
                 mstore(in, shl(224, 0xebcb0067)) // bands_x(int256)
                 mstore(add(in, 4), i)
-                success := staticcall(50000, pool, in, 0x24, add(res, add(p, 0x20)), 0x20)
+                success := staticcall(gas(), pool, in, 0x24, add(res, add(p, 0x20)), 0x20)
                 if eq(success, 0) {
                     revert(in, 0x24)
                 }
@@ -74,7 +74,7 @@ contract CurveLlammaHelper {
 
                 mstore(in, shl(224, 0x31f7e306)) // bands_y(int256)
                 mstore(add(in, 4), i)
-                success := staticcall(50000, pool, in, 0x24, add(res, add(p, 0x20)), 0x20)
+                success := staticcall(gas(), pool, in, 0x24, add(res, add(p, 0x20)), 0x20)
                 if eq(success, 0) {
                     revert(in, 0x24)
                 }
@@ -93,3 +93,4 @@ contract CurveLlammaHelper {
         }
     }
 }
+
