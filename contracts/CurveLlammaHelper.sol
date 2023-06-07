ed46664427f40dc65fd2ffe001258f656f8652f7
Mikhail Melnik <by.zumzoom@gmail.com>
2023-06-07 17:54:59 +0300

    simplify

--- contracts/CurveLlammaHelper.sol
+++ contracts/CurveLlammaHelper.sol
@@ -1,81 +1,68 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.19;
 
+
 contract CurveLlammaHelper {
-    function get(address pool) public view returns(bytes memory) {
+    // concat of [active_band(), min_band(), max_band(), price_oracle(), dynamic_fee(), admin_fee(), p_oracle_up()]
+    bytes32 private constant _SELECTORS = 0x8f8654c5ca72a821aaa615fc86fc88d377c34594fee3f7f92eb858e700000000;
+
+    function get(address pool) external view returns(bytes memory) {
         assembly {
             let pos := 0x120
-            let min
-            let max
-            let in := mload(0x40)
-            let res := add(in, 0x24)
+            let ptr := mload(0x40)
+            let res := add(ptr, 0x24)
             mstore(res, 0x20)
 
-            mstore(in, shl(224, 0x8f8654c5)) // active_band()
-            let success := staticcall(gas(), pool, in, 0x04, add(res, 0x40), 0x20)
-            if eq(success, 0) {
-                revert(in, 0x04)
-            }
+            mstore(ptr, _SELECTORS)
 
-            mstore(in, shl(224, 0x2eb858e7)) // p_oracle_up(int256)
-            mstore(add(in, 4), mload(add(res, 0x40)))
-            success := staticcall(gas(), pool, in, 0x24, add(res, 0x60), 0x20)
-            if eq(success, 0) {
-                revert(in, 0x24)
-            }
+            // call active_band()
+            if iszero(staticcall(gas(), pool, ptr, 0x04, add(res, 0x40), 0x20)) { revert(ptr, 0x04) }
 
-            mstore(in, shl(224, 0xca72a821)) // min_band()
-            success := staticcall(gas(), pool, in, 0x04, add(res, 0x80), 0x20)
-            if eq(success, 0) {
-                revert(in, 0x04)
-            }
-            min := mload(add(res, 0x80))
+            // copy result to p_oracle_up arg
+            mstore(add(ptr, 28), mload(add(res, 0x40)))
 
-            mstore(in, shl(224, 0xaaa615fc)) // max_band()
-            success := staticcall(gas(), pool, in, 0x04, add(res, 0xA0), 0x20)
-            if eq(success, 0) {
-                revert(in, 0x04)
-            }
-            max := mload(add(res, 0xA0))
+            // call p_oracle_up(active_band)
+            if iszero(staticcall(gas(), pool, add(ptr, 24), 0x24, add(res, 0x60), 0x20)) { revert(add(ptr, 24), 0x24) }
 
-            mstore(in, shl(224, 0x86fc88d3)) // price_oracle()
-            success := staticcall(gas(), pool, in, 0x04, add(res, 0xC0), 0x20)
-            if eq(success, 0) {
-                revert(in, 0x04)
-            }
+            // call min_band()
+            if iszero(staticcall(gas(), pool, add(ptr, 4), 0x04, add(res, 0x80), 0x20)) { revert(add(ptr, 4), 0x04) }
 
-            mstore(in, shl(224, 0x77c34594)) // dynamic_fee()
-            success := staticcall(gas(), pool, in, 0x04, add(res, 0xE0), 0x20)
-            if eq(success, 0) {
-                revert(in, 0x04)
-            }
+            let min := mload(add(res, 0x80))
 
-            mstore(in, shl(224, 0xfee3f7f9)) // admin_fee()
-            success := staticcall(gas(), pool, in, 0x04, add(res, 0x100), 0x20)
-            if eq(success, 0) {
-                revert(in, 0x04)
-            }
+            // call max_band()
+            if iszero(staticcall(gas(), pool, add(ptr, 8), 0x04, add(res, 0xA0), 0x20)) { revert(add(ptr, 8), 0x04) }
+
+            let max := mload(add(res, 0xA0))
+
+            // call price_oracle()
+            if iszero(staticcall(gas(), pool, add(ptr, 12), 0x04, add(res, 0xC0), 0x20)) { revert(add(ptr, 12), 0x04) }
+
+            // call dynamic_fee()
+            if iszero(staticcall(gas(), pool, add(ptr, 16), 0x04, add(res, 0xE0), 0x20)) { revert(add(ptr, 16), 0x04) }
+
+            // call admin_fee()
+            if iszero(staticcall(gas(), pool, add(ptr, 20), 0x04, add(res, 0x100), 0x20)) { revert(add(ptr, 20), 0x04) }
 
             for { let i := min } slt(i, max) { i := add(i, 1) } {
                 let c := and(i, 0x00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
                 let p := pos
 
-                mstore(in, shl(224, 0xebcb0067)) // bands_x(int256)
-                mstore(add(in, 4), i)
-                success := staticcall(gas(), pool, in, 0x24, add(res, add(p, 0x20)), 0x20)
-                if eq(success, 0) {
-                    revert(in, 0x24)
-                }
+                mstore(ptr, shl(224, 0xebcb0067))
+                mstore(add(ptr, 4), i)
+                // call bands_x(i)
+                if iszero(staticcall(gas(), pool, ptr, 0x24, add(res, add(p, 0x20)), 0x20)) { revert(ptr, 0x24) }
+
                 if gt(mload(add(res, add(p, 0x20))), 0) {
                     p := add(p, 0x20)
                     c := or(c, shl(255, 1))
                 }
 
-                mstore(in, shl(224, 0x31f7e306)) // bands_y(int256)
-                mstore(add(in, 4), i)
-                success := staticcall(gas(), pool, in, 0x24, add(res, add(p, 0x20)), 0x20)
-                if eq(success, 0) {
-                    revert(in, 0x24)
-                }
+                mstore(ptr, shl(224, 0x31f7e306))
+                mstore(add(ptr, 4), i)
+                // call bands_y(i)
+                if iszero(staticcall(gas(), pool, ptr, 0x24, add(res, add(p, 0x20)), 0x20)) { revert(ptr, 0x24) }
+
                 if gt(mload(add(res, add(p, 0x20))), 0) {
                     p := add(p, 0x20)
                     c := or(c, shl(254, 1))
@@ -86,9 +73,9 @@ contract CurveLlammaHelper {
                     pos := add(p, 0x20)
                 }
             }
+
             mstore(add(res, 0x20), sub(pos, 0x40))
             return (res, pos)
         }
     }
 }
-
