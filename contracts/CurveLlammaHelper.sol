aeb3a716d8f7590bc2be4435bc2735877d6cae9a
Mikhail Melnik <by.zumzoom@gmail.com>
2023-06-07 18:44:26 +0300

    rewrite a bit

--- contracts/CurveLlammaHelper.sol
+++ contracts/CurveLlammaHelper.sol
@@ -6,76 +6,93 @@ pragma solidity 0.8.19;
 contract CurveLlammaHelper {
     // concat of [active_band(), min_band(), max_band(), price_oracle(), dynamic_fee(), admin_fee(), p_oracle_up()]
     bytes32 private constant _SELECTORS = 0x8f8654c5ca72a821aaa615fc86fc88d377c34594fee3f7f92eb858e700000000;
+    bytes4 private constant _BANDS_X_SELECTOR = 0xebcb0067;
+    bytes4 private constant _BANDS_Y_SELECTOR = 0x31f7e306;
+    uint256 private constant _255_BIT_MASK = 0x8000000000000000000000000000000000000000000000000000000000000000;
+    uint256 private constant _254_BIT_MASK = 0x4000000000000000000000000000000000000000000000000000000000000000;
+    uint256 private constant _TWO_TOP_BITS_MASK = 0xc000000000000000000000000000000000000000000000000000000000000000;
+    uint256 private constant _MASK_WITHOUT_TWO_TOP_BITS = 0x3fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
-    function get(address pool) external view returns(bytes memory) {
+    function get(address pool) external view returns(bytes memory res) {
         assembly {
-            let pos := 0x120
             let ptr := mload(0x40)
-            let res := add(ptr, 0x24)
-            mstore(res, 0x20)
-
+            res := add(ptr, 0x40)
+            let resPtr := add(res, 0x20)
             mstore(ptr, _SELECTORS)
 
             // call active_band()
-            if iszero(staticcall(gas(), pool, ptr, 0x04, add(res, 0x40), 0x20)) { revert(ptr, 0x04) }
+            if iszero(staticcall(gas(), pool, ptr, 0x04, resPtr, 0x20)) { revert(ptr, 0x04) }
 
             // copy result to p_oracle_up arg
-            mstore(add(ptr, 28), mload(add(res, 0x40)))
+            mstore(add(ptr, 28), mload(resPtr))
+
+            resPtr := add(resPtr, 0x20)
 
             // call p_oracle_up(active_band)
-            if iszero(staticcall(gas(), pool, add(ptr, 24), 0x24, add(res, 0x60), 0x20)) { revert(add(ptr, 24), 0x24) }
+            if iszero(staticcall(gas(), pool, add(ptr, 24), 0x24, resPtr, 0x20)) { revert(add(ptr, 24), 0x24) }
+
+            resPtr := add(resPtr, 0x20)
 
             // call min_band()
-            if iszero(staticcall(gas(), pool, add(ptr, 4), 0x04, add(res, 0x80), 0x20)) { revert(add(ptr, 4), 0x04) }
+            if iszero(staticcall(gas(), pool, add(ptr, 4), 0x04, resPtr, 0x20)) { revert(add(ptr, 4), 0x04) }
 
-            let min := mload(add(res, 0x80))
+            let minBand := mload(resPtr)
+
+            resPtr := add(resPtr, 0x20)
 
             // call max_band()
-            if iszero(staticcall(gas(), pool, add(ptr, 8), 0x04, add(res, 0xA0), 0x20)) { revert(add(ptr, 8), 0x04) }
+            if iszero(staticcall(gas(), pool, add(ptr, 8), 0x04, resPtr, 0x20)) { revert(add(ptr, 8), 0x04) }
 
-            let max := mload(add(res, 0xA0))
+            let maxBand := mload(resPtr)
+
+            resPtr := add(resPtr, 0x20)
 
             // call price_oracle()
-            if iszero(staticcall(gas(), pool, add(ptr, 12), 0x04, add(res, 0xC0), 0x20)) { revert(add(ptr, 12), 0x04) }
+            if iszero(staticcall(gas(), pool, add(ptr, 12), 0x04, resPtr, 0x20)) { revert(add(ptr, 12), 0x04) }
+
+            resPtr := add(resPtr, 0x20)
 
             // call dynamic_fee()
-            if iszero(staticcall(gas(), pool, add(ptr, 16), 0x04, add(res, 0xE0), 0x20)) { revert(add(ptr, 16), 0x04) }
+            if iszero(staticcall(gas(), pool, add(ptr, 16), 0x04, resPtr, 0x20)) { revert(add(ptr, 16), 0x04) }
+
+            resPtr := add(resPtr, 0x20)
 
             // call admin_fee()
-            if iszero(staticcall(gas(), pool, add(ptr, 20), 0x04, add(res, 0x100), 0x20)) { revert(add(ptr, 20), 0x04) }
+            if iszero(staticcall(gas(), pool, add(ptr, 20), 0x04, resPtr, 0x20)) { revert(add(ptr, 20), 0x04) }
 
-            for { let i := min } slt(i, max) { i := add(i, 1) } {
-                let c := and(i, 0x00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
-                let p := pos
+            resPtr := add(resPtr, 0x20)
 
-                mstore(ptr, shl(224, 0xebcb0067))
+            for { let i := minBand } slt(i, maxBand) { i := add(i, 1) } {
+                let dataPtr := add(resPtr, 0x20)
+                let c := and(i, _MASK_WITHOUT_TWO_TOP_BITS)
+
+                mstore(ptr, _BANDS_X_SELECTOR)
                 mstore(add(ptr, 4), i)
                 // call bands_x(i)
-                if iszero(staticcall(gas(), pool, ptr, 0x24, add(res, add(p, 0x20)), 0x20)) { revert(ptr, 0x24) }
+                if iszero(staticcall(gas(), pool, ptr, 0x24, dataPtr, 0x20)) { revert(ptr, 0x24) }
 
-                if gt(mload(add(res, add(p, 0x20))), 0) {
-                    p := add(p, 0x20)
-                    c := or(c, shl(255, 1))
+                if mload(dataPtr) {
+                    dataPtr := add(dataPtr, 0x20)
+                    c := or(c, _255_BIT_MASK)
                 }
 
-                mstore(ptr, shl(224, 0x31f7e306))
+                mstore(ptr, _BANDS_Y_SELECTOR)
                 mstore(add(ptr, 4), i)
                 // call bands_y(i)
-                if iszero(staticcall(gas(), pool, ptr, 0x24, add(res, add(p, 0x20)), 0x20)) { revert(ptr, 0x24) }
+                if iszero(staticcall(gas(), pool, ptr, 0x24, dataPtr, 0x20)) { revert(ptr, 0x24) }
 
-                if gt(mload(add(res, add(p, 0x20))), 0) {
-                    p := add(p, 0x20)
-                    c := or(c, shl(254, 1))
+                if mload(dataPtr) {
+                    dataPtr := add(dataPtr, 0x20)
+                    c := or(c, _254_BIT_MASK)
                 }
 
-                if gt(p, pos) {
-                    mstore(add(res, pos), c)
-                    pos := add(p, 0x20)
+                if and(c, _TWO_TOP_BITS_MASK) {
+                    mstore(resPtr, c)
+                    resPtr := dataPtr
                 }
             }
 
-            mstore(add(res, 0x20), sub(pos, 0x40))
-            return (res, pos)
+            mstore(res, sub(sub(resPtr, res), 0x20))
         }
     }
 }
