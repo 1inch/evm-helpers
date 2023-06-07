26917fe4994d3d4f9b44763b426afca4a2d0f22a
Mikhail Melnik <by.zumzoom@gmail.com>
2023-06-07 11:59:10 +0300

    update LeftoverExchanger

--- contracts/LeftoverExchanger.sol
+++ contracts/LeftoverExchanger.sol
@@ -3,16 +3,29 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@1inch/solidity-utils/contracts/libraries/SafeERC20.sol";
+import "@1inch/solidity-utils/contracts/libraries/ECDSA.sol";
 
-
-contract LeftoverExchanger is Ownable {
-    event Action (bool success, address to, bytes result);
+/* solhint-disable avoid-low-level-calls */
+contract LeftoverExchanger is Ownable, IERC1271 {
+    using SafeERC20 for IERC20;
 
     struct Call {
         address to;
         uint256 value;
         bytes data;
     }
+
+    event CallFailure(uint256 i, bytes result);
+
+    error OnlyCreator();
+    error CallFailed(uint256 i, bytes result);
+    error InvalidLength();
+    error EstimationResults(bool[] statuses, bytes[] results);
+
+    address private immutable _creator = msg.sender;
 
     constructor(address owner_) {
         transferOwnership(owner_);
@@ -21,27 +34,53 @@ contract LeftoverExchanger is Ownable {
     // solhint-disable-next-line no-empty-blocks
     receive() external payable {}
 
-    // payable for paths with 0x
-    function makeCallsNoThrow(Call[] calldata calls) external payable onlyOwner {
-        uint256 startGas = gasleft();
-        for (uint i = 0; i < calls.length; i++) {
-            // solhint-disable-next-line avoid-low-level-calls
-            (bool ok, bytes memory result) = calls[i].to.call{value : calls[i].value}(calls[i].data);
-            emit Action(ok, calls[i].to, result);
-        }
-        uint256 gasRefund = (startGas - gasleft() + 21000 + (msg.data.length * 7) + 2000) * tx.gasprice;
-        if (address(this).balance >= gasRefund) {
-            (bool ok,) = payable(owner()).call{value: gasRefund}("");
-            require(ok, "refund failed");
+    function estimateMakeCalls(Call[] calldata calls) external payable onlyOwner {
+        unchecked {
+            bool[] memory statuses = new bool[](calls.length);
+            bytes[] memory results = new bytes[](calls.length);
+            for (uint256 i = 0; i < calls.length; i++) {
+                (statuses[i], results[i]) = calls[i].to.call{value : calls[i].value}(calls[i].data);
+            }
+            revert EstimationResults(statuses, results);
         }
     }
 
-    // payable for paths with 0x
-    function makeCalls(Call[] calldata calls) external payable onlyOwner {
-        for (uint i = 0; i < calls.length; i++) {
-            // solhint-disable-next-line avoid-low-level-calls
-            (bool ok,) = calls[i].to.call{value : calls[i].value}(calls[i].data);
-            require(ok, "swap failed");
+    function makeCallsNoThrow(Call[] calldata calls) external payable onlyOwner {
+        unchecked {
+            for (uint256 i = 0; i < calls.length; i++) {
+                (bool ok, bytes memory result) = calls[i].to.call{value : calls[i].value}(calls[i].data);
+                if (!ok) emit CallFailure(i, result);
+            }
         }
     }
+
+    function makeCalls(Call[] calldata calls) external payable onlyOwner {
+        unchecked {
+            for (uint256 i = 0; i < calls.length; i++) {
+                (bool ok, bytes memory result) = calls[i].to.call{value : calls[i].value}(calls[i].data);
+                if (!ok) revert CallFailed(i, result);
+            }
+        }
+    }
+
+    function batchApprove(bytes calldata data) external onlyOwner {
+        unchecked {
+            uint256 length = data.length;
+            if (length % 40 != 0) revert InvalidLength();
+            for (uint256 i = 0; i < length; i += 40) {
+                IERC20(address(bytes20(data[i:i+20]))).forceApprove(address(bytes20(data[i+20:i+40])), type(uint256).max);
+            }
+        }
+    }
+
+    function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4 magicValue) {
+        if (ECDSA.recover(hash, signature) == owner()) magicValue = this.isValidSignature.selector;
+    }
+
+    function destroy() external {
+        if (msg.sender != _creator) revert OnlyCreator();
+        selfdestruct(payable(this));
+    }
 }
+
+/* solhint-enable avoid-low-level-calls */
