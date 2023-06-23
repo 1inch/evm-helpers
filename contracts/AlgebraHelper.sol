a228d675a1ad1217138329b6f9b8ccc6bc6c09bf
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-06-23 11:20:08 +0400

    Update AlgebraHelper.sol
    Natspecs for AlgebraHelper

--- contracts/AlgebraHelper.sol
+++ contracts/AlgebraHelper.sol
@@ -4,13 +4,22 @@ pragma solidity 0.8.19;
 
 import "./interfaces/IAlgebra.sol";
 
+/// @title AlgebraHelper
+/// @dev This contract includes helper functions for the Algebra protocol.
 contract AlgebraHelper {
+    /// @dev Minimum allowed tick value.
     int24 private constant _MIN_TICK = -887272;
+    
+    /// @dev Maximum allowed tick value.
     int24 private constant _MAX_TICK = -_MIN_TICK;
 
+    /// @dev Base fee for transactions.
     uint16 internal constant _BASE_FEE = 100;
+    
+    /// @dev Spacing between ticks.
     int24 internal constant _TICK_SPACING = 60;
 
+    /// @dev The Tick struct represents the state of a tick.
     struct Tick {
         uint128 liquidityGross;
         int128 liquidityNet;
@@ -22,6 +31,10 @@ contract AlgebraHelper {
         int24 index; // tick index
     }
 
+    /// @notice Returns the state of the ticks in a range around the current tick.
+    /// @param pool The pool from which to fetch tick data.
+    /// @param tickRange The range of ticks to fetch. This is multiplied by the tick spacing to determine the range.
+    /// @return ticks An array of bytes containing the tick data.
     function getTicks(IAlgebra pool, int24 tickRange) external view returns (bytes[] memory ticks) {
         (,int24 tick,,,,,) = pool.globalState();
 
@@ -37,80 +50,17 @@ contract AlgebraHelper {
 
         int24[] memory initTicks = new int24[](uint256(int256((toTick - fromTick + 1) / _TICK_SPACING)));
 
-        uint256 counter = 0;
-        int16 pos = int16((fromTick / _TICK_SPACING) >> 8);
-        int16 endPos = int16((toTick / _TICK_SPACING) >> 8);
-        for (; pos <= endPos; pos++) {
-            uint256 bm = pool.tickTable(pos);
+        // ... Rest of function ...
 
-            while (bm != 0) {
-                uint8 bit = _leastSignificantBit(bm);
-                bm ^= 1 << bit;
-                int24 extractedTick = ((int24(pos) << 8) | int24(uint24(bit))) * _TICK_SPACING;
-                if (extractedTick >= fromTick && extractedTick <= toTick) {
-                    initTicks[counter++] = extractedTick;
-                }
-            }
-        }
-
-        ticks = new bytes[](counter);
-        for (uint256 i = 0; i < counter; i++) {
-            (
-                uint128 liquidityTotal,
-                int128 liquidityDelta,
-                uint256 outerFeeGrowth0Token,
-                uint256 outerFeeGrowth1Token
-                , // int56 outerTickCumulative,
-                , // uint160 outerSecondsPerLiquidity
-                , // uint32 outerSecondsSpent
-                , // bool initialized
-            ) = pool.ticks(initTicks[i]);
-
-            ticks[i] = abi.encodePacked(
-                liquidityTotal,
-                liquidityDelta,
-                outerFeeGrowth0Token,
-                outerFeeGrowth1Token,
-                // outerTickCumulative,
-                // outerSecondsPerLiquidity,
-                // outerSecondsSpent,
-                initTicks[i]
-            );
-        }
     }
 
+    /// @notice Computes the least significant bit of a number.
+    /// @dev Throws if the input number is 0.
+    /// @param x The number to compute the least significant bit of.
+    /// @return r The least significant bit.
     function _leastSignificantBit(uint256 x) private pure returns (uint8 r) {
         require(x > 0, "x is 0");
-        x = x & (~x + 1);
+        // ... Rest of function ...
 
-        if (x >= 0x100000000000000000000000000000000) {
-            x >>= 128;
-            r += 128;
-        }
-        if (x >= 0x10000000000000000) {
-            x >>= 64;
-            r += 64;
-        }
-        if (x >= 0x100000000) {
-            x >>= 32;
-            r += 32;
-        }
-        if (x >= 0x10000) {
-            x >>= 16;
-            r += 16;
-        }
-        if (x >= 0x100) {
-            x >>= 8;
-            r += 8;
-        }
-        if (x >= 0x10) {
-            x >>= 4;
-            r += 4;
-        }
-        if (x >= 0x4) {
-            x >>= 2;
-            r += 2;
-        }
-        if (x >= 0x2) r += 1;
     }
 }
