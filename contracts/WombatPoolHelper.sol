36e28aa7181d0b93bfa4b75636709d6fe0c91218
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-06-23 12:30:40 +0400

    Update WombatPoolHelper.sol
    Update Naatspecs

--- contracts/WombatPoolHelper.sol
+++ contracts/WombatPoolHelper.sol
@@ -4,8 +4,20 @@ pragma solidity 0.8.19;
 
 import "./interfaces/IMasterWombatV3.sol";
 
+
+/// @title WombatPoolHelper
+/// @dev Helper contract to interact with the Master Wombat V3 contract and fetch information about the various pools.
 contract WombatPoolHelper {
+
+    /// @dev Instance of the IMasterWombatV3 interface.
     IMasterWombatV3 private constant _WOMBAT_MASTER_V3 = IMasterWombatV3(0x489833311676B566f888119c29bd997Dc6C95830);
+
+    /// @notice Returns an array of information about all pools.
+    /// @dev Iterates over all LP tokens and pools, fetches and returns arrays of related information.
+    /// @return pools An array of pool addresses.
+    /// @return poolTokens A two-dimensional array where each element is an array of token addresses for a specific pool.
+    /// @return lpTokens An array of all LP token addresses.
+    /// @return underlyingTokens An array of all underlying token addresses associated with the LP tokens.
 
     function getAllPoolData() external view returns (address[] memory pools, address[][] memory poolTokens, address[] memory lpTokens, address[] memory underlyingTokens) {
         // poolLength is actually the number of LP tokens tracked
