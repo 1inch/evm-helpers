d14318e85c4fc033fdcd912570831c98d235a0b5
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-06-23 12:24:17 +0400

    Update TraderJoeHelper_v2_1.sol
    Updated TraderJoeHelper Natspecs

--- contracts/TraderJoeHelper_v2_1.sol
+++ contracts/TraderJoeHelper_v2_1.sol
@@ -4,13 +4,27 @@ pragma solidity 0.8.19;
 
 import "./interfaces/ILBPair.sol";
 
+/// @title TraderJoeHelper_v2_1
+/// @dev Helper contract for interacting with TraderJoe pair contracts.
 // solhint-disable-next-line contract-name-camelcase
 contract TraderJoeHelper_v2_1 {
+
+    /// @dev Struct to hold data about a liquidity bin.
     struct BinData {
         uint256 id;
         uint256 reserveX;
         uint256 reserveY;
     }
+
+    /// @notice Retrieves information about specific bins in an ILBPair contract.
+    /// @dev Iterates over bins of a pair starting from offset to last non-empty bin or until size bins have been collected.
+    ///      Then, collects reserves and id of each non-empty bin and stores them in a struct.
+    ///      If end of the bins is reached before size bins are collected, the loop wraps around and starts from the first bin.
+    /// @param pair The ILBPair contract to retrieve bin data from.
+    /// @param offset The bin id to start collecting data from.
+    /// @param size The maximum number of bins to collect data from.
+    /// @return data An array of BinData structs containing id and reserves for each collected bin.
+    /// @return i The bin id where data collection stopped. It is reset to 0 if end of bins was reached before collecting size bins.
 
     function getBins(ILBPair pair, uint24 offset, uint24 size)
         external
