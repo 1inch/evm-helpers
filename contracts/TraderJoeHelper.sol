4734a4c92d424fcfe4e5eaddc53bbd82c1656875
Gleb Alekseev <alekseev.gleb@gmail.com>
2023-06-28 10:46:46 +0300

    Update TraderJoeHelper.sol

--- contracts/TraderJoeHelper.sol
+++ contracts/TraderJoeHelper.sol
@@ -5,26 +5,24 @@ pragma solidity 0.8.19;
 import "./interfaces/IJoePair.sol";
 
 /// @title TraderJoeHelper
-/// @dev Helper contract for interacting with TraderJoe pair contracts.
+/// @notice A helper contract for interacting with Trader Joe's liquidity pool pairs.
 contract TraderJoeHelper {
 
-    /// @dev Struct to hold data about a liquidity bin.
+    /// @dev Represents data about a bin in a Trader Joe pair.
     struct BinData {
         uint256 id;
         uint256 reserveX;
         uint256 reserveY;
     }
 
-    /// @notice Retrieves information about specific bins in a JoePair contract.
-    /// @dev Iterates over bins of a pair starting from offset to last non-empty bin or until size bins have been collected.
-    ///      Then, collects reserves and id of each non-empty bin and stores them in a struct.
-    ///      If end of the bins is reached before size bins are collected, the loop wraps around and starts from the first bin.
-    /// @param pair The JoePair contract to retrieve bin data from.
-    /// @param offset The bin id to start collecting data from.
-    /// @param size The maximum number of bins to collect data from.
-    /// @return data An array of BinData structs containing id and reserves for each collected bin.
-    /// @return i The bin id where data collection stopped. It is reset to 0 if end of bins was reached before collecting size bins.
-
+    /**
+     * @notice Fetches data about a range of bins in a given Trader Joe pair.
+     * @param pair The Trader Joe pair to fetch bin data from.
+     * @param offset The ID of the first bin to fetch data from.
+     * @param size The maximum number of bins to fetch data for.
+     * @return data An array of BinData structs containing data about each bin.
+     * @return i The bin id where data collection stopped. It is reset to 0 if end of bins was reached before collecting size bins.
+     */
     function getBins(IJoePair pair, uint24 offset, uint24 size)
         external
         view
