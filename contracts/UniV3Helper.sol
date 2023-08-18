cf03a17ecc6a7904eed7d6d7823d6efa4fc577eb
Mikhail Melnik <by.zumzoom@gmail.com>
2023-08-18 13:36:02 +0400

    redeploy helper on base

--- contracts/UniV3Helper.sol
+++ contracts/UniV3Helper.sol
@@ -8,17 +8,6 @@ contract UniV3Helper {
     int24 private constant _MIN_TICK = -887272;
     int24 private constant _MAX_TICK = -_MIN_TICK;
 
-    struct Tick {
-        uint128 liquidityGross;
-        int128 liquidityNet;
-        uint256 feeGrowthOutside0X128;
-        uint256 feeGrowthOutside1X128;
-        int56 tickCumulativeOutside;
-        uint160 secondsPerLiquidityOutsideX128;
-        uint32 secondsOutside;
-        int24 index; // tick index
-    }
-
     function getTicks(IUniswapV3 pool, int24 tickRange) external view returns (bytes[] memory ticks) {
         int24 tickSpacing = pool.tickSpacing();
         (,int24 tick) = pool.slot0();
