624a4fdbf9725e17e59a291f4543d92b3ff0d2c2
Denis <zzomrot.ne@gmail.com>
2023-10-14 00:29:56 +0300

    Some fixes

--- contracts/SolidlyV3Helper.sol
+++ contracts/SolidlyV3Helper.sol
@@ -1,11 +1,13 @@
-pragma solidity ^0.7.6;
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
 
 pragma abicoder v2;
 
 interface ISolidlyV3 {
-    
+
     function tickSpacing() external view returns (int24);
-    
+
     function slot0()
         external
         view
@@ -43,49 +45,50 @@ interface ISolidlyV3 {
 
 
 contract SolidlyV3Helper {
-    
-    int24 private constant MIN_TICK = -887272;
-    int24 private constant MAX_TICK = -MIN_TICK;
-    
+
+    error ZeroInputError();
+
+    int24 private constant _MIN_TICK = -887272;
+    int24 private constant _MAX_TICK = -_MIN_TICK;
+
     function getTicks(ISolidlyV3 pool, int24 tickRange) external view returns (bytes[] memory ticks) {
-        
+
         int24 tickSpacing = pool.tickSpacing();
         (,int24 tick,,) = pool.slot0();
-        
+
         int24 fromTick = tick - (tickSpacing * tickRange);
         int24 toTick = tick + (tickSpacing * tickRange);
-        if (fromTick < MIN_TICK) {
-            fromTick = MIN_TICK;
+        if (fromTick < _MIN_TICK) {
+            fromTick = _MIN_TICK;
         }
-        if (toTick > MAX_TICK) {
-            toTick = MAX_TICK;
+        if (toTick > _MAX_TICK) {
+            toTick = _MAX_TICK;
         }
-        
-        int24[] memory initTicks = new int24[](uint((toTick - fromTick + 1) / tickSpacing));
+
+        int24[] memory initTicks = new int24[](uint256(int256(toTick - fromTick + 1) / int256(tickSpacing)));
 
         uint counter = 0;
         for (int24 tickNum = (fromTick / tickSpacing * tickSpacing); tickNum <=  (toTick / tickSpacing * tickSpacing); tickNum += (256 * tickSpacing)) {
             int16 pos = int16((tickNum / tickSpacing) >> 8);
-            uint256 bm = pool.tickBitmap(pos);   
-        
-             while (bm != 0) {
-                 uint8 bit = mostSignificantBit(bm);
-                 initTicks[counter] = (int24(pos) * 256 + int24(bit)) * tickSpacing;
-                 
-                 counter += 1;
-                 bm ^= 1 << bit;
-             }
-             
+            uint256 bm = pool.tickBitmap(pos);
+
+            while (bm != 0) {
+                uint8 bit = _mostSignificantBit(bm);
+                initTicks[counter] = (int24(pos) * 256 + int24(int256(uint256(bit)))) * tickSpacing;
+
+                counter += 1;
+                bm ^= 1 << bit;
+            }
         }
-        
+
         ticks = new bytes[](counter);
         for (uint i = 0; i < counter; i++) {
              ticks[i] = abi.encodePacked(initTicks[i]);
         }
     }
-    
-    function mostSignificantBit(uint256 x) private pure returns (uint8 r) {
-        require(x > 0);
+
+    function _mostSignificantBit(uint256 x) private pure returns (uint8 r) {
+        if (x == 0) revert ZeroInputError();
 
         if (x >= 0x100000000000000000000000000000000) {
             x >>= 128;
