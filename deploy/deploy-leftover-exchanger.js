66a9da30f6a22af00c6529c6eb6c380d8f7df35b
Sunnesoft <n.galikhanov@1inch.io>
2025-09-22 10:38:08 +0200

    refactored leftover exchanger deployment script

--- deploy/deploy-leftover-exchanger.js
+++ deploy/deploy-leftover-exchanger.js
@@ -1,53 +1,48 @@
+const { deployAndGetContractWithCreate3, deployAndGetContract } = require('@1inch/solidity-utils');
 const hre = require('hardhat');
 const { getChainId, ethers } = hre;
 const constants = require('./constants');
 
 const LEFTOVER_EXCHANGER_SALT = ethers.keccak256(ethers.toUtf8Bytes('LeftoverExchanger'));
-
 const ADMIN_SLOT = '0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103';
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
+    const networkName = hre.network.name;
     console.log('running deploy script');
     const chainId = await getChainId();
     console.log('network id ', chainId);
 
+    if (
+        networkName in hre.config.networks &&
+        chainId !== hre.config.networks[networkName].chainId?.toString()
+    ) {
+        console.log(`network chain id: ${hre.config.networks[networkName].chainId}, your chain id ${chainId}`);
+        console.log('skipping wrong chain id deployment');
+        return;
+    }
+
     const { deployer } = await getNamedAccounts();
-    const { deploy } = deployments;
 
-    const leftoverExchangerImpl = await deploy(
-        'LeftoverExchangerImpl',
-        { args: [constants.WETH[chainId], constants.LEFTOVER_EXCHANGER_OWNER[chainId]], from: deployer, contract: 'LeftoverExchanger' },
-    );
-    console.log('LeftoverExchangerImpl deployed to:', leftoverExchangerImpl.address);
-
-    const create3Deployer = await ethers.getContractAt('ICreate3Deployer', constants.CREATE3_DEPLOYER_CONTRACT[chainId]);
-
-    const TransparentProxyFactory = await ethers.getContractFactory('TransparentUpgradeableProxy');
-
-    const deployData = (await TransparentProxyFactory.getDeployTransaction(
-        leftoverExchangerImpl.address,
+    const leftoverExchangerImpl = await deployAndGetContract({
+        contractName: 'LeftoverExchanger',
+        deploymentName: 'LeftoverExchangerImpl',
+        deployments,
         deployer,
-        '0x',
-    )).data;
+        constructorArgs: [constants.WETH[chainId], constants.LEFTOVER_EXCHANGER_OWNER[chainId]],
+    });
 
-    const deployTxn = await create3Deployer.deploy(LEFTOVER_EXCHANGER_SALT, deployData);
-    await deployTxn.wait();
+    const transparentUpgradeableProxy = await deployAndGetContractWithCreate3({
+        contractName: 'TransparentUpgradeableProxy',
+        deploymentName: 'LeftoverExchanger',
+        constructorArgs: [await leftoverExchangerImpl.getAddress(), deployer, '0x'],
+        create3Deployer: constants.CREATE3_DEPLOYER_CONTRACT[chainId],
+        salt: LEFTOVER_EXCHANGER_SALT,
+        deployments,
+    });
 
-    console.log(`LeftoverExchanger proxy deployed to: ${await create3Deployer.addressOf(LEFTOVER_EXCHANGER_SALT)}`);
-
-    if (await getChainId() !== '31337') {
-        await hre.run('verify:verify', {
-            address: await create3Deployer.addressOf(LEFTOVER_EXCHANGER_SALT),
-            constructorArguments: [leftoverExchangerImpl.address, deployer, '0x'],
-        });
-
-        await hre.run('verify:verify', {
-            address: leftoverExchangerImpl.address,
-            constructorArguments: [constants.WETH[chainId], constants.LEFTOVER_EXCHANGER_OWNER[chainId]],
-        });
-
+    if (chainId !== '31337') {
         const proxyAdminBytes32 = await ethers.provider.send('eth_getStorageAt', [
-            await create3Deployer.addressOf(LEFTOVER_EXCHANGER_SALT),
+            await transparentUpgradeableProxy.getAddress(),
             ADMIN_SLOT,
             'latest',
         ]);
