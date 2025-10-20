c4f52ad6fcc238aa7ec2072b3ab75fe47c86be6c
Ilya Petrusenko <ilyapt4@gmail.com>
2025-10-20 12:35:11 +0200

    calcLimits gets params and reserves by its own

--- contracts/EulerLimitsHelper.sol
+++ contracts/EulerLimitsHelper.sol
@@ -2,6 +2,29 @@
 
 pragma solidity ^0.8.0;
 
+interface EulerPool {
+       struct Params {
+       // Entities
+       address vault0;
+       address vault1;
+       address eulerAccount;
+       // Curve
+       uint112 equilibriumReserve0;
+       uint112 equilibriumReserve1;
+       uint256 priceX;
+       uint256 priceY;
+       uint256 concentrationX;
+       uint256 concentrationY;
+       // Fees
+       uint256 fee;
+       uint256 protocolFee;
+       address protocolFeeRecipient;
+   }
+
+    function getParams() external view returns (Params memory);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 status);
+}
+
 interface IEVault {
     function debtOf(address account) external view returns (uint256);
     function maxDeposit(address account) external view returns (uint256);
@@ -15,19 +38,14 @@ interface IEVault {
 contract EulerLimitsHelper {
     uint256 private constant _MAX_U112 = type(uint112).max;
 
-    function calcLimits(
-        address eulerAccount,
-        address _vault0,
-        address _vault1,
-        uint112 reserve0,
-        uint112 reserve1
-    ) external view returns (uint256, uint256, uint256, uint256) {
-        return (
-            _calcInLimit(eulerAccount, IEVault(_vault0)),
-            _calcInLimit(eulerAccount, IEVault(_vault1)),
-            _calcOutLimit(eulerAccount, IEVault(_vault0), reserve0),
-            _calcOutLimit(eulerAccount, IEVault(_vault1), reserve1)
-        );
+    function calcLimits(EulerPool _pool) external view returns (uint256, uint256, uint256, uint256) {
+            EulerPool.Params memory params = _pool.getParams();
+            (uint112 reserve0, uint112 reserve1,) = _pool.getReserves();
+
+            IEVault vault0 = IEVault(params.vault0);
+            IEVault vault1 = IEVault(params.vault1);
+            return (_calcInLimit(params.eulerAccount, vault0), _calcInLimit(params.eulerAccount, vault1),
+                    _calcOutLimit(params.eulerAccount, vault0, reserve0), _calcOutLimit(params.eulerAccount, vault1, reserve1));
     }
 
     function _calcInLimit(address eulerAccount, IEVault vault) internal view returns (uint256) {
