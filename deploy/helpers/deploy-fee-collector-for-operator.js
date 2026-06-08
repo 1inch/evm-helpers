e214e0f4997863ba527c20d9a6d7df92d39ccf1a
Gleb Alekseev <alekseev.gleb@gmail.com>
2026-06-08 13:59:54 -0300

    fix async dep

--- deploy/helpers/deploy-fee-collector-for-operator.js
+++ deploy/helpers/deploy-fee-collector-for-operator.js
@@ -12,22 +12,19 @@ async function deployFeeCollectorForOperator(hre, chainId, feeCollectorOperatorN
     console.log(`Using salt ${salt} for operator ${feeCollectorOperatorName}`);
 
     const feeCollectorFactory = await ethers.getContractAt('FeeCollectorFactory', constants.FEE_COLLECTOR_FACTORY[chainId]);
-    await feeCollectorFactory.deployFeeCollector(salt);
+    const deployTx = await feeCollectorFactory.deployFeeCollector(salt);
+    await deployTx.wait();
+
     const feeCollectorAddress = await feeCollectorFactory.getFeeCollectorAddress(salt);
     console.log('FeeCollector deployed at', feeCollectorAddress);
 
-    if (chainId !== '31337' && process.env.OPS_SKIP_VERIFY !== 'true') {
-        await hre.run('verify:verify', {
-            address: feeCollectorAddress,
-            constructorArguments: [constants.FEE_COLLECTOR_FACTORY[chainId], '0x'],
-        });
-    }
-
     const feeCollector = await ethers.getContractAt('FeeCollector', feeCollectorAddress);
-    await feeCollector.setOperator(operatorAddress);
-    console.log('feeCollectorOperator set to', feeCollectorAddress);
+    tx = await feeCollector.setOperator(operatorAddress);
+    await tx.wait();
+    console.log('feeCollectorOperator set to', operatorAddress);
 
     return feeCollectorAddress;
 }
 
 module.exports = { deployFeeCollectorForOperator };
+module.exports.skip = async () => true;
